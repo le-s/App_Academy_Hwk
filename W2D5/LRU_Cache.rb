7c49a54dc5c26d2f2e97e4ad1215c61c30c10a5d
Steven Le <steven.an.le@gmail.com>
2018-11-02 00:22:32 -0700

    Create functioning cache to add new elements and delete old elements

--- W2D5/LRU_Cache.rb
+++ W2D5/LRU_Cache.rb
@@ -1,17 +1,27 @@
 class LRUCache
-    def initialize
+    def initialize(size)
+      @size = size
+      @cache = []
     end
 
     def count
-      # returns number of elements currently in cache
+      @cache.count
     end
 
     def add(el)
-      # adds element to cache according to LRU principle
+      if @cache.include?(el)
+        @cache.delete(el)
+        @cache << el
+      elsif @cache.count >= @size
+        @cache.shift
+        @cache << el
+      else
+        @cache << el
+      end
     end
 
     def show
-      # shows the items in the cache, with the LRU item first
+      @cache
     end
 
     private
