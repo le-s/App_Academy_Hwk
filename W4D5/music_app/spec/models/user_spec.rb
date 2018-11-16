c377e0e3e45d26a8f53133db488b4ea56a14edef
Steven Le <steven.an.le@gmail.com>
2018-11-15 20:49:39 -0800

    Add specs for password_digest and session_token

--- W4D5/music_app/spec/models/user_spec.rb
+++ W4D5/music_app/spec/models/user_spec.rb
@@ -4,5 +4,14 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name)}
     it { should validate_presence_of(:password_digest)}
-  end
+    it { should validate_length_of(:password).is_at_least(6)}
+
+    it "creates a password digest when a password is given" do
+     expect(user.password_digest).to_not be_nil
+   end
+
+   it "creates a session token before validation" do
+     user.valid?
+     expect(user.session_token).to_not be_nil
+   end
 end
